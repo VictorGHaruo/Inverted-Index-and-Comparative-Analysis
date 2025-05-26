38deea57709d47ee0cae12a76b6bc126df93bc86
Derik <dekribeiro7@gmail.com>
2025-05-26 14:03:09 -0300

    lendo até 9

--- scr/main_bst.cpp
+++ scr/main_bst.cpp
@@ -7,46 +7,66 @@
 
 using namespace std;
 
+bool isPosInt(string strNum){
+    int inter = 0;
+    if(strNum[0] == '-'){
+        return false;
+    } else if (strNum[0] == '+'){
+        inter = 1;
+    }
+    int size = strNum.size();
+    for(int i = inter; i < size; i++){
+        if(!isdigit(strNum[i])) return false;
+    }
+    //if not found other char means that is all numbers but all 0's, thus not positive even if the fist char is '+'
+    if (strNum.find_first_not_of('0', inter) == string::npos) return false; 
+    
+    return true;
+}
+
 int main(int argc, char** argv) {
     
+    // Validate arguments
     if (argc != 4) {
         cerr << "Usage: " << argv[0] << " <comand> <n_docs> <directory_path>" << endl;
         return 1;
     }
-
     string comand = argv[1];
     if (!(comand == "search" || comand == "stats")) {
         cerr << "Usage: " << argv[0] << " <comand> <n_docs> <directory_path>" << endl;
-        cerr << "The arg <comand> must be 'search' or 'stats'." << endl;
+        cerr << "Error : The arg <comand> must be 'search' or 'stats'." << endl;
         return 1;
     }
-
     int numMax = -1;
-    try{
-        int numMax = stoi(argv[2]);
-    } catch (...) {
-        cerr << "Algo de errado não está certo." << endl;
-    } 
-    if(numMax < 0) {
-        cerr << "Algo de errado não está certo. 2" << endl;
+    if (isPosInt(argv[2])){
+        numMax = stoi(argv[2]);
+    } else {
+        cerr << "Usage: " << argv[0] << " <comand> <n_docs> <directory_path>" << endl;
+        cerr << "Error : <n_docs> must be a positive integer." << endl;;
         return 1;
     }
     auto texts = data::readData(argv[3], numMax);
-    
     if (texts.empty()){
-        cerr << "Directory path is invalid." << endl;
+        cerr << "Usage: " << argv[0] << " <comand> <n_docs> <directory_path>" << endl;
+        cerr << "Error : Directory path is invalid." << endl;
         return 1;
     } else if(texts.size() < numMax){ //opcional
-        cerr << "n_docs is greater than the number of txt's in the directory." << endl;
+        cerr << "Warning : n_docs is greater than the number of txt's in the directory." << endl;
     }
-    
-    //Só por agr
-    string a = "Teste";
-    vector<int> b = {1};
 
-    Node* fistnode = createNode(a, b, nullptr);
-    BinaryTree* bst = createTree(fistnode);
+    // Populate the tree 
+    BinaryTree* bst = BST::create();
 
+    int sizeTexts = texts.size();
+    for(int i = 0; i < sizeTexts; i++){
+        int sizeT = texts[i].size();
+        for(int j = 0; j < sizeT; j++){
+            cout << i << endl;
+            BST::insert(bst, texts[i][j], i);
+        }
+    }
+
+    // Commands
     if (comand == "search"){
         
         cout << "OIII" << endl;
